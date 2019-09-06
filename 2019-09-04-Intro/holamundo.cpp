1a505a67081154bb12367c3ebdd5fb4d28e7ba84
miauriafernanda <miauriafernanda@MacBook-Air.local>
2019-09-06 11:28:08 -0500

    My first commit

---
+++ 2019-09-04-Intro/holamundo.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+//using namespace std;
+
+int main (void)
+{
+  double x= 1.0e-6;
+  //std::cout<<"Hola mundo"<<std::endl;
+  std::cout<< x <<std::endl;
+  //std::cout<<"Hola mundo \n";
+  return 0;
+}
